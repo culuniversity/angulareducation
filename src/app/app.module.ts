f429684ee2800675996c09b171030a93780a9057
Omar Marneco <omar@omarenco.com>
2020-04-08 20:11:29 -0500

    template Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthModule} from './auth/auth.module';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {ForgetPasswordComponent} from './auth/forget-password/forget-password.component';
+import {SmartTemplateComponent} from './template/smart-template/smart-template.component';
+import {TemplateModule} from './template/template.module';
+import {DashboardModule} from './pages/dashboard/dashboard.module';
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( response => response.AuthModule )
+  },
+  {
+    path: 'pages',
+    component: SmartTemplateComponent,
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then(response => response.DashboardModule)
+  }
+]
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    AuthModule,
+    TemplateModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]

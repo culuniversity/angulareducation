0770696ac49e6eb370c6aa764882ba551012867d
john Hernandez <johnjairohernandez@gmail.com>
2020-04-17 17:16:36 -0500

    se creo componente profile user con su ruta y link a bootstrap

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {ProfileComponent} from '../users/profile/profile.component';
 
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
   }
 ];
 
